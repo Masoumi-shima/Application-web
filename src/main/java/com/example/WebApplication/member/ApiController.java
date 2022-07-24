b5f3471d6f8f430300e5df2b8e9559f3cc115877
Masoumi-shima <masoumi.shimaa@gmail.com>
2022-07-24 11:48:22 -0400

    Add validation for PUT method.

--- src/main/java/com/example/WebApplication/member/ApiController.java
+++ src/main/java/com/example/WebApplication/member/ApiController.java
@@ -1,12 +1,21 @@
 package com.example.WebApplication.member;
 
+import com.github.fge.jsonpatch.JsonPatch;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.ReflectUtils;
+import org.springframework.data.web.JsonPath;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.sound.midi.Patch;
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Controller
 public class ApiController
@@ -48,7 +57,7 @@ public class ApiController
     }
 
     @PutMapping("/api/membres/:{permitNumber}")
-    public ResponseEntity<Member> updateMember(@PathVariable("permitNumber") String permitNumber, @RequestBody Member updatedMember)
+    public ResponseEntity<Member> updateMember(@PathVariable("permitNumber") String permitNumber,@Validated  @RequestBody Member updatedMember)
     {
         if (!memberService.isPermitNumberValid(permitNumber))
         {
@@ -64,6 +73,32 @@ public class ApiController
             return ResponseEntity.notFound().build();
         }
     }
+
+//    @PatchMapping(path = "/api/membres/:{permitNumber}", consumes = "application/json-patch+json")
+//    public ResponseEntity<Member> updateMemberPartially(@PathVariable("permitNumber") String permitNumber,
+//                                                        @RequestBody String json)
+//    {
+//        Optional<Member> member = memberRepository.findById(permitNumber);
+//        if (!memberService.isPermitNumberValid(permitNumber))
+//        {
+//            return ResponseEntity.badRequest().build();
+//        }
+//        if (member.isPresent())
+//        {
+//            fields.forEach((key, value) ->
+//            {
+//                Field field = ReflectionUtils.findField(Member.class, (String) key);
+//                field.setAccessible(true);
+//                ReflectionUtils.setField(field, member.get(), value);
+//            });
+//            memberRepository.save(member.get());
+//            return ResponseEntity.ok(member.get());
+//        }
+//        else
+//        {
+//            return ResponseEntity.notFound().build();
+//        }
+//    }
 
     @DeleteMapping("/api/membres/:{permitNumber}")
     public ResponseEntity<Member> deleteMember(@PathVariable("permitNumber") String permitNumber)
@@ -85,7 +120,7 @@ public class ApiController
     public ResponseEntity<Member> createMember(@Validated @RequestBody Member member)
     {
         memberRepository.save(member);
-        return ResponseEntity.ok(member);
+        return new ResponseEntity<Member>(member,HttpStatus.CREATED);
     }
 }
 
